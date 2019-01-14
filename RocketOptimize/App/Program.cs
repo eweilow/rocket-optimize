6ffca3c10eafc092d72fcaa8c69e8eff8db5073c
E. Weilow <eweilow@digitalkoppar.se>
2019-01-14 13:57:24 +0100

    Render test

--- RocketOptimize/App/Program.cs
+++ RocketOptimize/App/Program.cs
@@ -1,33 +1,19 @@
-﻿using RocketOptimize.Simulation;
+﻿using OpenTK.Graphics.OpenGL;
+using RocketOptimize.App.Render;
+using RocketOptimize.Simulation;
 using RocketOptimize.Simulation.Integrators;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace RocketOptimize.App
 {
-    class Program
+    class Program : Window
     {
-        static void Main(string[] args)
+        static State InitializeState(float v0, float theta)
         {
-            var controlVectors = new Input[]{
-                new Input(90f, 1f),
-                new Input(80f, 1f),
-                new Input(70f, 1f),
-                new Input(60f, 1f),
-                new Input(50f, 1f),
-                new Input(40f, 1f),
-                new Input(30f, 1f),
-                new Input(20f, 1f),
-                new Input(10f, 1f),
-                new Input(0f,  1f)
-            };
-
-            var simulation = new AscentSimulation();
-            simulation.Run(controlVectors);
-
-            var theta = 0.25f;
-            var v0 = 555f;
-            var currentState = new State()
+            return new State()
             {
                 Position = new Vector3()
                 {
@@ -42,33 +28,90 @@ namespace RocketOptimize.App
                     Z = 0
                 }
             };
+        }
 
-            var rk4 = new RK4();
+        State currentState = InitializeState(555f, 0.45f);
+        RK4 rk4 = new RK4();
 
+        List<State> states = new List<State>();
 
-            while (currentState.Position.Y >= -0.1f)
+        public int Rate = 1;
+        public int MicroStepping = 1;
+
+        public override void DidResize(int width, int height)
+        {
+            Camera.Resize(5000, 5000);
+            Camera.SetProjectionOrthographic(-1f, 1f);
+        }
+
+        public override void UpdateTick(float updateTime)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < Rate; i++)
             {
-                rk4.Integrate(0.0166666f, ref currentState, out currentState, (State state) =>
+                for (int j = 0; j < MicroStepping; j++)
                 {
-                    return Vector3.UnitY * (-9.81f);
-                });
+                    rk4.Integrate(updateTime / MicroStepping, ref currentState, out currentState, (State state) =>
+                    {
+                        if (state.Position.Y < -0.1f)
+                        {
+                            return -state.Velocity * 10f;
+                        }
+                        return Vector3.UnitY * (-9.81f) - 0.001f * state.Velocity * state.Velocity.Length();
+                    });
+                }
+                states.Add(currentState);
             }
-            Console.WriteLine("{0} ({1,2:F} {2,2:F}) {3,2:F} ({4,2:F})", currentState.Time, currentState.Position.X, currentState.Position.Y, currentState.Velocity.X, currentState.Velocity.Y);
-            Console.WriteLine("{0} ({1,2:F} {2,2:F}) {3,2:F} ({4,2:F})",
-                currentState.Time,
-                v0 * Math.Cos(theta) * currentState.Time,
-                v0 * Math.Sin(theta) * currentState.Time - 0.5 * 9.81 * currentState.Time * currentState.Time,
-                v0 * Math.Cos(theta),
-                v0 * Math.Sin(theta) - 9.81 * currentState.Time
-            );
-            Console.WriteLine("{0} ({1,2:F} {2,2:F}) {3,2:F} ({4,2:F})",
-                currentState.Time,
-                v0 * Math.Cos(theta) * currentState.Time - currentState.Position.X,
-                v0 * Math.Sin(theta) * currentState.Time - 0.5 * 9.81 * currentState.Time * currentState.Time - currentState.Position.Y,
-                v0 * Math.Cos(theta) - currentState.Velocity.X,
-                v0 * Math.Sin(theta) - 9.81 * currentState.Time - currentState.Velocity.Y
-            );
-            Console.ReadLine();
+            watch.Stop();
+            double ticks = watch.ElapsedTicks;
+            double seconds = ticks / Stopwatch.Frequency;
+
+            Title = string.Format("{0,2:F}", seconds * 1000);
+
+            Camera.SetPosition(new OpenTK.Vector3() { X = currentState.Position.X, Y = currentState.Position.Y, Z = currentState.Position.Z });
+            Camera.SetLookat(new OpenTK.Vector3() { X = currentState.Position.X, Y = currentState.Position.Y, Z = currentState.Position.Z - 1 });
+        }
+
+        public override void RenderTick(float updateTime)
+        {
+            // Console.WriteLine("{0} ({1,2:F} {2,2:F}) {3,2:F} ({4,2:F})", 1f / updateTime, currentState.Position.X, currentState.Position.Y, currentState.Velocity.X, currentState.Velocity.Y);
+
+            GL.LineWidth(4f);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(-5000f, 0f, 0f);
+            GL.Vertex3(5000f, 0f, 0f);
+            GL.End();
+
+            GL.LineWidth(1f);
+            GL.Begin(PrimitiveType.Lines);
+            for (float f = 1f; f < 100f; f += 1f)
+            {
+                GL.Color3(0.1f, 0.1f, 0.1f);
+                GL.Vertex3(-5000f, f*1000f, 0f);
+                GL.Vertex3(5000f, f*1000f, 0f);
+            }
+            GL.End();
+
+            GL.Begin(PrimitiveType.LineStrip);
+            foreach (var state in states)
+            {
+                GL.Color3(1f, 1f, 1f);
+                GL.Vertex3(state.Position.X, state.Position.Y, state.Position.Z);
+            }
+            GL.End();
+        }
+
+
+        static void Main(string[] args)
+        {
+            using (var window = new Program())
+            {
+                window.Rate = 1;
+                window.MicroStepping = 100;
+                window.Start(60.0);
+            }
         }
     }
 }
---
+++ RocketOptimize/App/Render/Window.cs
@@ -0,0 +1,69 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+
+namespace RocketOptimize.App.Render
+{
+    public abstract class Window : GameWindow
+    {
+        public readonly Camera Camera;
+
+        public Window(int width = 800, int height = 600, string title = "Rocket Optimize") : base(width, height)
+        {
+            Title = title;
+            VSync = VSyncMode.On;
+
+            Camera = new Camera();
+            Camera.SetProjectionOrthographic(-1f, 1f);
+            Camera.SetPosition(Vector3.Zero);
+            Camera.SetLookat(-Vector3.UnitZ);
+        }
+
+        public void Start(double updateRate)
+        {
+            Run(updateRate);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            OnResize(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            UpdateTick((float)e.Time);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            DidResize(Width, Height);
+
+
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            
+            var viewMatrix = Camera.ViewMatrix;
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref viewMatrix);
+
+            var projectionMatrix = Camera.ProjectionMatrix;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projectionMatrix);
+
+            RenderTick((float)e.Time);
+            SwapBuffers();
+        }
+
+        public abstract void UpdateTick(float updateTime);
+        public abstract void RenderTick(float updateTime);
+        public abstract void DidResize(int width, int height);
+    }
+}
