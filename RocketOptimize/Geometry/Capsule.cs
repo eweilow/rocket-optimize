6341ef7275cce52ee5df2fb6b2d6dc136df5b0f5
E. Weilow <eweilow@digitalkoppar.se>
2019-01-14 11:40:19 +0100

    Initial geometry package

---
+++ RocketOptimize/Geometry/Capsule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketOptimize.Geometry
+{
+
+    public class Capsule : IVolume
+    {
+        public readonly double Radius;
+        public readonly double Height;
+
+        public Capsule(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        public double GetShellArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetShellVolume(double thickness)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RocketOptimize/Geometry/IVolume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketOptimize.Geometry
+{
+    public interface IVolume
+    {
+        double GetVolume();
+        double GetShellArea();
+        double GetShellVolume(double thickness);
+    }
+}
