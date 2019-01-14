a079d5024402d4c4276f6a3a7670e6cf32378300
E. Weilow <eweilow@digitalkoppar.se>
2019-01-14 13:11:57 +0100

    Port Camera

---
+++ RocketOptimize/App/Render/Camera.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Numerics;
+
+namespace RocketOptimize.App.Render
+{
+    public enum ProjectionType
+    {
+        Perspective,
+        Ortographic
+    }
+
+    public class Camera
+    {
+        public ProjectionType ProjectionType { get; private set; }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public float FovRads
+        {
+            get
+            {
+                return (Fov * (float)Math.PI) / 180f;
+            }
+        }
+
+        public float Fov { get; private set; }
+
+        public float NearZ { get; private set; }
+        public float FarZ { get; private set; }
+
+        public Matrix4x4 ProjectionMatrix { get; private set; }
+        public Matrix4x4 ViewMatrix { get; private set; }
+
+        public Vector3 Position { get; private set; }
+        public Vector3 LookAt { get; private set; }
+
+        private void Update()
+        {
+            ViewMatrix = Matrix4x4.CreateLookAt(Position, LookAt, Vector3.UnitY);
+
+            switch (ProjectionType)
+            {
+                case ProjectionType.Perspective:
+                    ProjectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(FovRads, Width / (float)Height, NearZ, FarZ);
+                    break;
+                case ProjectionType.Ortographic:
+                    var halfWidth = Width / 2f;
+                    var halfHeight = Height / 2f;
+                    ProjectionMatrix = Matrix4x4.CreateOrthographicOffCenter(-halfWidth, halfWidth, -halfHeight, halfHeight, NearZ, FarZ);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
+
+        public void Resize(int w, int h)
+        {
+            Width = w;
+            Height = h;
+            Update();
+        }
+
+        public void SetPosition(Vector3 position)
+        {
+            Position = position;
+            Update();
+        }
+
+        public void SetLookat(Vector3 lookat)
+        {
+            LookAt = lookat;
+            Update();
+        }
+
+        public void SetProjectionPerspective(float fov, float near, float far)
+        {
+            ProjectionType = ProjectionType.Perspective;
+            Fov = fov;
+            NearZ = near;
+            FarZ = far;
+            Update();
+        }
+
+        public void SetProjectionOrthographic(float near, float far)
+        {
+            ProjectionType = ProjectionType.Ortographic;
+            NearZ = near;
+            FarZ = far;
+            Update();
+        }
+    }
+}
