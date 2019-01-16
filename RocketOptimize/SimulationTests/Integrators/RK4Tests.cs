a446222fea3189ad8f924b8daea4de017c7ec130
E. Weilow <eweilow@digitalkoppar.se>
2019-01-16 08:53:35 +0100

    Update tests

--- RocketOptimize/SimulationTests/Integrators/RK4Tests.cs
+++ RocketOptimize/SimulationTests/Integrators/RK4Tests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK;
 using System;
-using System.Numerics;
 
 namespace RocketOptimize.Simulation.Integrators.Tests
 {
@@ -16,13 +16,13 @@ namespace RocketOptimize.Simulation.Integrators.Tests
 
             var currentState = new State()
             {
-                Position = new Vector3()
+                Position = new Vector3d()
                 {
                     X = 0,
                     Y = 0,
                     Z = 0
                 },
-                Velocity = new Vector3()
+                Velocity = new Vector3d()
                 {
                     X = v0 * (float)Math.Cos(theta),
                     Y = v0 * (float)Math.Sin(theta),
@@ -36,25 +36,25 @@ namespace RocketOptimize.Simulation.Integrators.Tests
             const float allowedDeviationPosition = 0.8f;
             const float allowedDeviationVelocity = 0.02f;
 
-            Vector3 deltaPosition = Vector3.Zero;
-            Vector3 deltaVelocity = Vector3.Zero;
+            Vector3d deltaPosition = Vector3d.Zero;
+            Vector3d deltaVelocity = Vector3d.Zero;
 
             var rk4 = new RK4();
             while (currentState.Position.Y >= -0.1f)
             {
-                rk4.Integrate(timeStep, ref currentState, out currentState, (State state) =>
+                rk4.Integrate(timeStep, ref currentState, out currentState, (ref State state) =>
                 {
-                    return Vector3.UnitY * Gravity;
+                    return Vector3d.UnitY * Gravity;
                 });
 
-                Vector3 analyticalPosition = new Vector3()
+                Vector3d analyticalPosition = new Vector3d()
                 {
                     X = (float)(v0 * Math.Cos(theta) * currentState.Time),
                     Y = (float)(v0 * Math.Sin(theta) * currentState.Time - 0.5 * 9.81 * currentState.Time * currentState.Time),
                     Z = 0f
                 };
 
-                Vector3 analyticalVelocity = new Vector3()
+                Vector3d analyticalVelocity = new Vector3d()
                 {
                     X = (float)(v0 * Math.Cos(theta)),
                     Y = (float)(v0 * Math.Sin(theta) - 9.81 * currentState.Time),
@@ -64,16 +64,16 @@ namespace RocketOptimize.Simulation.Integrators.Tests
                 deltaPosition = analyticalPosition - currentState.Position;
                 deltaVelocity = analyticalVelocity - currentState.Velocity;
 
-                if (deltaPosition.Length() > allowedDeviationPosition)
+                if (deltaPosition.Length > allowedDeviationPosition)
                 {
-                    Assert.Fail(string.Format("Position has gone out of error range at time {1,2:F}: {0,8:F}", deltaPosition.Length(), currentState.Time));
+                    Assert.Fail(string.Format("Position has gone out of error range at time {1,2:F}: {0,8:F}", deltaPosition.Length, currentState.Time));
                 }
-                if (deltaVelocity.Length() > allowedDeviationVelocity)
+                if (deltaVelocity.Length > allowedDeviationVelocity)
                 {
-                    Assert.Fail(string.Format("Velocity has gone out of error range at time {1,2:F}: {0,8:F}", deltaVelocity.Length(), currentState.Time));
+                    Assert.Fail(string.Format("Velocity has gone out of error range at time {1,2:F}: {0,8:F}", deltaVelocity.Length, currentState.Time));
                 }
             }
-            Console.WriteLine(string.Format("Final deviation: position: {0,8:F}, velocity: {1,8:F}", deltaPosition.Length(), deltaVelocity.Length()));
+            Console.WriteLine(string.Format("Final deviation: position: {0,8:F}, velocity: {1,8:F}", deltaPosition.Length, deltaVelocity.Length));
         }
     }
 }
